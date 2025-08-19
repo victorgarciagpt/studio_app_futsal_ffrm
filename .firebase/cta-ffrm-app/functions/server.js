07c076d823d206dd3a80cb25eb7bff03888d5a3b
victor garcia acosta <victorgarciagpt@gmail.com>
2025-08-19 11:30:37 +0000

    Ignorar carpeta .firebase y eliminarla del índice

--- .firebase/cta-ffrm-app/functions/server.js
+++
@@ -1,4 +0,0 @@
-const { onRequest } = require('firebase-functions/v2/https');
-  const server = import('firebase-frameworks');
-  exports.ssrctaffrmapp = onRequest({"region":"europe-west1"}, (req, res) => server.then(it => it.handle(req, res)));
-  
